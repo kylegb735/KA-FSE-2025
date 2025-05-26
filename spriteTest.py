f15fd11098903e68c5e3b42641c31ccc699f6ad0
kylegb735 <kylegb735@gmail.com>
2025-05-26 13:51:37 -0400

    damage/kill function. Remove bg from enemies

--- spriteTest.py
+++ spriteTest.py
@@ -22,16 +22,11 @@ def getPics(sprite, moves):
         enemyPics.append(movePics)
     return enemyPics
 
-def updateSprite(sprite):
-    sprite[FRAME] += 0.2
-    idle = sprite[MOVES].index('Idle')
+def generateEnemy():
+    type = randint(0, len(enemyTypes) - 1) # randomly select an enemy type
+    enemies.append([randint(100, 1100), randint(100, 700), enemyTypes[type][0], enemyTypes[type][1], enemyTypes[type][2], enemyTypes[type][3], enemyTypes[type][4], enemyTypes[type][5]])
 
-    if sprite[FRAME] >= len(sprite[PICS][sprite[MOVE]]):
-        sprite[FRAME] = 0
-        sprite[MOVE] = idle
-    print(sprite[MOVE], sprite[FRAME])
-
-def doAttack(sprite, move):
+def doMove(sprite, move):
     index = sprite[MOVES].index(move)
     idle = sprite[MOVES].index('Idle')
     if sprite[MOVE] == idle:
@@ -40,6 +35,23 @@ def doAttack(sprite, move):
     elif sprite[FRAME] >= len(sprite[PICS][sprite[MOVE]]):
         sprite[FRAME] = 0
         sprite[MOVE] = idle
+
+def damage(sprite, amount):
+    sprite[HEALTH] -= amount
+    if sprite[HEALTH] <= 0:
+        sprite[HEALTH] = 0
+
+def kill(sprite):
+    enemies.remove(sprite)
+
+def updateSprite(sprite):
+    sprite[FRAME] += 0.2
+    idle = sprite[MOVES].index('Idle')
+
+    if sprite[FRAME] >= len(sprite[PICS][sprite[MOVE]]):
+        sprite[FRAME] = 0
+        sprite[MOVE] = idle
+    print(sprite[MOVE], sprite[FRAME])
 
 def drawSprite(sprite):
     pic = sprite[PICS][sprite[MOVE]][int(sprite[FRAME])]
@@ -52,31 +64,25 @@ HEALTH = 3
 MOVES = 4
 PICS = 5
 
-#            X   Y   Move          Speed
-berserker = ['berserker', 5   , 0, 100]
+#            name         move  frame health
+berserker = ['berserker', 6   , 0   , 130]
 berserker.append(getMoves(berserker[NAME]))
 berserker.append(getPics(berserker[NAME], berserker[MOVES]))
 
-shaman = ['shaman', 4   , 0, 100]
+shaman = ['shaman', 6   , 0, 70]
 shaman.append(getMoves(shaman[NAME]))
 shaman.append(getPics(shaman[NAME], shaman[MOVES]))
 
-warrior = ['warrior', 5   , 0, 100]
+warrior = ['warrior', 6   , 0, 100]
 warrior.append(getMoves(warrior[NAME]))
 warrior.append(getPics(warrior[NAME], warrior[MOVES]))
 
 enemyTypes = [berserker, shaman, warrior]
 enemies = []
-enemiesTemp = []
-enemiesPos = []
 for i in range(30):
-    enemiesPos.append([randint(100,1100), randint(100,700)])
-    enemiesTemp.append(choice(enemyTypes).copy())
-for i in range(30):
-    enemies.append([enemiesPos[i][0],enemiesPos[i][1], enemiesTemp[i][NAME], enemiesTemp[i][MOVE], enemiesTemp[i][FRAME], enemiesTemp[i][HEALTH], enemiesTemp[i][MOVES], enemiesTemp[i][PICS]])
-    # enemies.append([randint(100,1100), randint(100,700)],choice(enemyTypes).copy())
-
+    generateEnemy()
 print(enemies)
+
 X = 0
 Y = 1
 NAME = 2
@@ -90,23 +96,33 @@ frame = 0
 running = True
 gameClock = time.Clock()
 while running:
+    mbd = False
     for evnt in event.get():
         if evnt.type == QUIT:
             running = False
+        if evnt.type == MOUSEBUTTONDOWN:
+            mbd = True
     mb = mouse.get_pressed()
     keys = key.get_pressed()
     screen.fill((255,255,255))
 
-    if mb[0]:
-        for enemy in enemies[:15]:
-            doAttack(enemy, 'Attack_1')
-    if mb[2]:
-        for enemy in enemies[15:]:
-            doAttack(enemy, 'Attack_1')
+    if mbd and mb[0]:
+        for enemy in enemies:
+            doMove(enemy, 'Attack_4')
+    if mbd and mb[2]:
+        for enemy in enemies:
+            doMove(enemy, 'Hurt')
+            damage(enemy, 15)
+
 
     for enemy in enemies:
+        if enemy[HEALTH] <= 0:
+            kill(enemy)
         updateSprite(enemy)
         drawSprite(enemy)
+
+    if len(enemies) < 30:
+        generateEnemy()
 
     gameClock.tick(50)
     display.flip()
